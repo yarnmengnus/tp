15d2f51b870af7533a986cfa7da20ad26a07ca54
yarnmengnus <lam.yarn.meng@gmail.com>
2023-10-17 09:36:15 +0800

    Fix more checkstyle issues

--- src/main/java/profplan/logic/parser/AddCommandParser.java
+++ src/main/java/profplan/logic/parser/AddCommandParser.java
@@ -33,7 +33,7 @@ public class AddCommandParser implements Parser<AddCommand> {
      */
     public AddCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, 
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE,
                 PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG, PREFIX_LINK);
 
         if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)
--- src/main/java/profplan/logic/parser/EditCommandParser.java
+++ src/main/java/profplan/logic/parser/EditCommandParser.java
@@ -32,7 +32,7 @@ public class EditCommandParser implements Parser<EditCommand> {
     public EditCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, 
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE,
                 PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG, PREFIX_LINK);
 
         Index index;
