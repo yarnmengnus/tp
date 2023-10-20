fd17d8d6d5624c072612e9c0f34a0787cea94dac
Jackie <jackieguo126@gmail.com>
2023-10-20 22:21:26 +0800

    Code coverage

---
+++ src/test/java/profplan/logic/commands/FilterCommandTest.java
@@ -0,0 +1,50 @@
+package profplan.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import profplan.model.task.DueDate;
+import profplan.model.task.TasksDueBeforeDatePredicate;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code FilterCommand}.
+ */
+public class FilterCommandTest {
+    @Test
+    public void equals() {
+        TasksDueBeforeDatePredicate firstPredicate =
+                new TasksDueBeforeDatePredicate(new DueDate("01-01-2000"));
+        TasksDueBeforeDatePredicate secondPredicate =
+                new TasksDueBeforeDatePredicate(new DueDate("02-02-2002"));
+
+        FilterCommand filterFirstCommand = new FilterCommand(firstPredicate);
+        FilterCommand filterSecondCommand = new FilterCommand(secondPredicate);
+
+        // same object -> returns true
+        assertTrue(filterFirstCommand.equals(filterFirstCommand));
+
+        // same values -> returns true
+        FilterCommand filterFirstCommandCopy = new FilterCommand(firstPredicate);
+        assertTrue(filterFirstCommand.equals(filterFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(filterFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(filterFirstCommand.equals(null));
+
+        // different task -> returns false
+        assertFalse(filterFirstCommand.equals(filterSecondCommand));
+    }
+
+    @Test
+    public void toStringMethod() {
+        TasksDueBeforeDatePredicate predicate = new TasksDueBeforeDatePredicate(new DueDate("01-01-2000"));
+        FilterCommand filterCommand = new FilterCommand(predicate);
+        String expected = FilterCommand.class.getCanonicalName() + "{date predicate=" + predicate + "}";
+        assertEquals(expected, filterCommand.toString());
+    }
+}
