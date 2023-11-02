77b670d01751437a3bbf3f995cf6ef389a89c704
yarnmengnus <lam.yarn.meng@gmail.com>
2023-11-03 00:33:46 +0800

    Fix bugs in link

--- src/main/java/profplan/model/task/Link.java
+++ src/main/java/profplan/model/task/Link.java
@@ -18,9 +18,7 @@ public class Link {
             + "    - end with a domain label at least 2 characters long\n"
             + "    - have each domain label start and end with alphanumeric characters\n"
             + "    - have each domain label consist of alphanumeric characters, separated only by hyphens, if any.";
-    public static final String VALIDATION_REGEX = "^(https?|ftp|file)://"
-            + "[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
-
+    public static final String VALIDATION_REGEX = "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$";
     public final String value;
 
     /**
