e9181328af05adcb16c3765966870dee490b5b6c
newway1814 <e1088145@u.nus.edu>
2023-11-09 17:02:27 +0800

    Add test cases for better code coverage

--- src/test/java/profplan/logic/commands/UnmarkCommandTest.java
+++ src/test/java/profplan/logic/commands/UnmarkCommandTest.java
@@ -11,12 +11,13 @@ import profplan.model.ModelManager;
 import profplan.model.task.Task;
 import profplan.testutil.Assert;
 import profplan.testutil.TaskBuilder;
-public class MarkCommandTest {
+
+public class UnmarkCommandTest {
 
 
     @Test
     public void constructor_taskNumberAsZero_throwsNullPointerException() {
-        Assert.assertThrows(AssertionError.class, () -> new MarkCommand(0));
+        Assert.assertThrows(AssertionError.class, () -> new UnmarkCommand(0));
     }
 
     @Test
@@ -26,54 +27,54 @@ public class MarkCommandTest {
 
         modelStub.addTask(validTask);
 
-        assertThrows(AssertionError.class, ()-> new MarkCommand(-1).execute(modelStub));
+        assertThrows(AssertionError.class, ()-> new UnmarkCommand(-1).execute(modelStub));
     }
 
     @Test
     public void execute_negativeIndexToMark_throwsAssertionFailureException() {
         int outOfBoundIndex = -100;
-        assertThrows(AssertionError.class, ()-> new MarkCommand(outOfBoundIndex));
+        assertThrows(AssertionError.class, ()-> new UnmarkCommand(outOfBoundIndex));
     }
 
     @Test
     public void execute_zeroIndexToMark_throwsAssertionFailureException() {
         int outOfBoundIndex = 0;
-        assertThrows(AssertionError.class, ()-> new MarkCommand(outOfBoundIndex));
+        assertThrows(AssertionError.class, ()-> new UnmarkCommand(outOfBoundIndex));
     }
 
 
     @Test
     public void testEqualsWithVariousScenarios() {
-        MarkCommand markCommand1 = new MarkCommand(1);
-        MarkCommand markCommand2 = new MarkCommand(1);
-        MarkCommand markCommand3 = new MarkCommand(2);
+        UnmarkCommand unmarkCommand1 = new UnmarkCommand(1);
+        UnmarkCommand markCommand2 = new UnmarkCommand(1);
+        UnmarkCommand markCommand3 = new UnmarkCommand(2);
         Object notMarkCommand = new Object();
 
 
-        assertTrue(markCommand1.equals(markCommand1));
+        assertTrue(unmarkCommand1.equals(unmarkCommand1));
 
         // Test for null
-        assertFalse(markCommand1.equals(null));
+        assertFalse(unmarkCommand1.equals(null));
 
 
-        assertTrue(markCommand1.equals(markCommand2));
-        assertTrue(markCommand2.equals(markCommand1));
+        assertTrue(unmarkCommand1.equals(markCommand2));
+        assertTrue(markCommand2.equals(unmarkCommand1));
 
 
-        MarkCommand markCommand4 = new MarkCommand(1);
-        assertTrue(markCommand1.equals(markCommand2));
+        UnmarkCommand markCommand4 = new UnmarkCommand(1);
+        assertTrue(unmarkCommand1.equals(markCommand2));
         assertTrue(markCommand2.equals(markCommand4));
-        assertTrue(markCommand1.equals(markCommand4));
+        assertTrue(unmarkCommand1.equals(markCommand4));
 
 
-        assertTrue(markCommand1.equals(markCommand2));
-        assertTrue(markCommand1.equals(markCommand2));
+        assertTrue(unmarkCommand1.equals(markCommand2));
+        assertTrue(unmarkCommand1.equals(markCommand2));
 
 
-        assertFalse(markCommand1.equals(markCommand3));
+        assertFalse(unmarkCommand1.equals(markCommand3));
 
 
-        assertFalse(markCommand1.equals(notMarkCommand));
+        assertFalse(unmarkCommand1.equals(notMarkCommand));
     }
 }
 
---
+++ src/test/java/profplan/logic/parser/UnmarkCommandParserTest.java
@@ -0,0 +1,47 @@
+package profplan.logic.parser;
+
+import org.junit.jupiter.api.Test;
+
+import profplan.logic.commands.UnmarkCommand;
+
+public class UnmarkCommandParserTest {
+
+    private UnmarkCommandParser parser = new UnmarkCommandParser();
+    @Test
+    public void parse_validArgs_returnsMarkCommand() {
+        CommandParserTestUtil.assertParseSuccess(parser, "1", new UnmarkCommand(1));
+    }
+
+    @Test
+    public void parse_negativeNumber_throwsParseException() {
+        CommandParserTestUtil.assertParseFailure(parser, "-1", UnmarkCommand.MESSAGE_INVALID_NUMBER);
+    }
+    @Test
+    public void parse_zero_throwsParseException() {
+        CommandParserTestUtil.assertParseFailure(parser, "0", UnmarkCommand.MESSAGE_INVALID_NUMBER);
+    }
+
+    @Test
+    public void parse_nonNumeric_throwsParseException() {
+        CommandParserTestUtil.assertParseFailure(parser, "a", String.format(
+            UnmarkCommand.MESSAGE_USAGE + UnmarkCommand.MESSAGE_DETAILS + UnmarkCommand.MESSAGE_EXAMPLE));
+    }
+
+    @Test
+    public void parse_emptyString_throwsParseException() {
+        CommandParserTestUtil.assertParseFailure(parser, "", String.format(
+            UnmarkCommand.MESSAGE_USAGE + UnmarkCommand.MESSAGE_DETAILS + UnmarkCommand.MESSAGE_EXAMPLE));
+    }
+
+    @Test
+    public void parse_whitespaceOnly_throwsParseException() {
+        CommandParserTestUtil.assertParseFailure(parser, "   ", String.format(
+            UnmarkCommand.MESSAGE_USAGE + UnmarkCommand.MESSAGE_DETAILS + UnmarkCommand.MESSAGE_EXAMPLE));
+    }
+
+    @Test
+    public void parse_validNumberWithWhitespace_returnsMarkCommand() {
+        CommandParserTestUtil.assertParseSuccess(parser, " 3 ", new UnmarkCommand(3));
+    }
+
+}
