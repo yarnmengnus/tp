d85e0f48b90e4f49aa6d80288cd08c4a7ecb4bee
kiatkat <kiatkat123@gmail.com>
2023-11-12 17:41:44 +0800

    Fix parsing bug

--- src/main/java/profplan/logic/parser/EditSettingsCommandParser.java
+++ src/main/java/profplan/logic/parser/EditSettingsCommandParser.java
@@ -1,6 +1,9 @@
 package profplan.logic.parser;
 
 import static java.util.Objects.requireNonNull;
+import static profplan.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.stream.Stream;
 
 import profplan.commons.core.Settings;
 import profplan.logic.commands.EditSettingsCommand;
@@ -19,8 +22,14 @@ public class EditSettingsCommandParser implements Parser<EditSettingsCommand> {
     @Override
     public EditSettingsCommand parse(String userInput) throws ParseException {
         requireNonNull(userInput);
+
         Prefix[] keywords = Settings.KEYWORDS;
         ArgumentMultimap argumentMultimap = ArgumentTokenizer.tokenize(userInput, keywords);
+
+        if (!isPrefixPresent(argumentMultimap, keywords) || !argumentMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    EditSettingsCommand.MESSAGE_FULL_HELP));
+        }
 
         EditSettingsCommand.EditSettingsDescriptor editSettingsDescriptor =
                 new EditSettingsCommand.EditSettingsDescriptor();
@@ -32,5 +41,13 @@ public class EditSettingsCommandParser implements Parser<EditSettingsCommand> {
         }
 
         return new EditSettingsCommand(editSettingsDescriptor);
+    }
+
+    /**
+     * Returns true if at least one of the prefixes does not contain empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean isPrefixPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).anyMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
